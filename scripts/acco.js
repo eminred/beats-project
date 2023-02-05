f9773f0a3d00761fa5e74b5749b96cb14383b117
Emin <eminsamedovred@gmail.com>
2023-02-05 17:11:57 +0300

    red triangle added to the team

---
+++ scripts/acco.js
@@ -0,0 +1,32 @@
+const openItem = item => {
+  const container = item.closest(".team__item");
+  const contentBlock = container.find(".team__content");
+  const textBlock = contentBlock.find(".team__content-block");
+  const reqHeight = textBlock.height();
+
+  container.addClass("active");
+  contentBlock.height(reqHeight);
+}
+
+const closeEveryItem = container => {
+  const items = container.find('.team__content');
+  const itemContainer = container.find(".team__item");
+
+  itemContainer.removeClass("active");
+  items.height(0);
+}
+
+$('.team__title').click(e =>{
+  const $this = $(e.currentTarget);
+  const container = $this.closest('.team');
+  const elemContainer = $this.closest(".team__item");
+
+  if (elemContainer.hasClass("active")) {
+    closeEveryItem(container);
+  } else {
+    closeEveryItem(container);
+    openItem($this);
+  }
+});
+
+
